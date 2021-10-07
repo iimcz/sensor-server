e28db1de16d065ea59d5dd9b48bb3d217f584d8a
Martin Karlik <karlima1@cvut.cz>
2021-10-07 10:22:44 +0200

    Fix wrong coordinates update

--- Hand.cs
+++ Hand.cs
@@ -1,11 +1,9 @@
-using System;
-
 namespace DepthCamera{
     class Hand
     {
-        private static readonly int _verticalTolerance = 1;
         public int X;
         public int Y;
+        private static readonly int _verticalTolerance = 1;
         public Hand(float x, float y)
         {
             X = (int)(x * 10);
@@ -15,8 +13,24 @@ namespace DepthCamera{
         public HandMovement CalculateMovement(Hand hand)
         {
             if(X == hand.X && Y == hand.Y) return HandMovement.None;
-            if(Math.Abs(X - hand.X) <= _verticalTolerance && Y < hand.Y) return HandMovement.Down;
-            if(Math.Abs(X - hand.X) <= _verticalTolerance && Y > hand.Y) return HandMovement.Up;
+            if(X == hand.X && Y < hand.Y) return HandMovement.Down;
+            if(X == hand.X + _verticalTolerance && Y < hand.Y){
+                hand.X -= _verticalTolerance;
+                return HandMovement.Down;
+            }
+            if(X == hand.X - _verticalTolerance && Y < hand.Y){
+                hand.X += _verticalTolerance;
+                return HandMovement.Down;
+            }
+            if(X == hand.X && Y > hand.Y) return HandMovement.Up;
+            if(X == hand.X + _verticalTolerance && Y > hand.Y){
+                hand.X -= _verticalTolerance;
+                return HandMovement.Up;
+            }
+            if(X == hand.X - _verticalTolerance && Y > hand.Y){
+                hand.X += _verticalTolerance;
+                return HandMovement.Up;
+            }
             if(X < hand.X && Y == hand.Y) return HandMovement.Right;
             if(X > hand.X && Y == hand.Y) return HandMovement.Left;
             return HandMovement.None;
