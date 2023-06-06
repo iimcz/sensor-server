c03342a22f8e3d57f55898f50f3a786c6cffe840
Martin Karlík <karlima1@fit.cvut.cz>
2023-06-06 14:21:39 +0200

    Removed unused gesture detector

--- Configuration/DepthCameraConfiguration.cs
+++ Configuration/DepthCameraConfiguration.cs
@@ -5,10 +5,7 @@ namespace SensorServer.Configuration
         public string BackgroundMode { get; set; } = "dynamic"; // static_first_frame, dynamic
         public int CalibrationFramesNumber { get; set; } = 20; // Number of frame to calibrate static backround
         public int GestureDelay { get; set; } = 1000; // In ms
-        public int GestureLength { get; set; } = 2; 
         public float JointMinConfidence { get; set; } = 0.75f; // Minimal joint confident
-        public float HorizontalGridSize { get; set; } = 0.1f; 
-        public float VerticalGridSize { get; set; } = 0.1f;
         public int MaxUsers { get; set; } = 6;
         public bool SendSkeletonData { get; set; } = false;
         public int BestUserChangeDelay { get; set; } = 1000; //In ms
